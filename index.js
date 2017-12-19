50616b0cc307811a835c167ecb46a35ecb6e256f
marcoslhc <marcoslhc@gmail.com>
2017-12-18 21:13:40 -0500

    add simple templating

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ function compose(...fns) {
 
 }
 
+const homepage = (ctx) => `
+  <html>
+    <link rel="stylesheet" href="https://unpkg.com/normalize.css@7.0.0/normalize.css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <style>
+      * {
+        font-family: 'Lato'
+      }
+    </style>
+    <body>
+      <h1>${ctx.message}</h1>
+    </body>
+  </html>
+`;
+
 http.createServer(function (req, res) {
-  body = `
-    <html>
-      <link rel="stylesheet" href="https://unpkg.com/normalize.css@7.0.0/normalize.css" />
-      <body>
-        <h1>Hello World</h1>
-      </body>
-    </html>
-  `;
+  body = homepage({
+    message: "Hello World"
+  });
   res.setHeader('content-type', 'text/html');
   res.setHeader('content-length', body.length);
   res.end(body);
